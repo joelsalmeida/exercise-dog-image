cb6eed326080f08a62c04a242ae5b7dc012ad3f5
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-11-10 10:22:59 -0300

    Cria estado "dogList", responsavel por armazanar lista de imagens. Cria funçao "fetchDogImage", responsavel pela requisiçao de uma imagem de cachorro aleatoria. fetchDogImage e chamada dentro de componentDidMount

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,28 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      dogList: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchDogImage();
+  }
+
+  async fetchDogImage() {
+    const url = 'https://dog.ceo/api/breeds/image/random';
+    const response = await fetch(url);
+    const data = await response.json();
+    const { message } = data;
+
+    this.setState((prevState) => ({
+      dogList: [...prevState.dogList, message],
+    }));
+  }
+
   render() {
     return (
       <main className="dog-image-app">
