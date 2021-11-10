8784aa73eb7fb2bb388c1a1ea456333350397d86
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-11-10 10:00:59 -0300

    Cria estrutura base do componente app - html/css

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <main className="dog-image-app">
+        <h1>Dog Image App</h1>
+        <section className="dog-images-section">
+          Dog images Section
+        </section>
+      </main>
+    );
+  }
+}
+
+export default App;
